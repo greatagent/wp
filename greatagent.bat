b3cbe47ab5cdaaa91323e380cc9203cd002572c8
wwqgtxx <wwqgtxx@gmail.com>
2013-11-09 09:41:05 +0800

    bat

--- greatagent.bat
+++ greatagent.bat
@@ -1,1 +1,6 @@
+:: greatagent-ga.bat
+:: Main Batch File
+   
+@echo off
+title greatagent-wp
 greatagent-wp.bat
--- runner.bat
+++ runner.bat
@@ -1,25 +1,16 @@
 @echo off
-:: clean-old-file.inc.bat
-:: Step4 - Old file cleanup
-
-utility\php\php.exe -c utility\php\php.ini clean-old-file.php
-
-utility\sleep.exe -m 1000
-
-:: startwallproxy.inc.bat
-:: Step5 - Start WallProxy
-
+cd /D "%~dp0"
+:: startgoagent.inc.bat
+:: Step2 - Start GoAgent
 echo Starting WallProxy...
 start wallproxy-local\wallproxy.exe
 
-:: get-last-kown-good.inc.bat
-:: Step6 - Start get-last-kown-good
-utility\php\php.exe -c utility\php\php.ini get-last-kown-good.inc.php
 
 :: startfirefox.inc.bat
-:: Step7 - Start Firefox
-echo Starting FirefoxPortable...
-utility\php\php.exe -c utility\php\php.ini startfirefox.inc.php
+:: Step3 - Start PortableBroswer
+echo Starting PortableBroswer...
+python27.exe startbroswer.py
 
 pause
 
+exit
